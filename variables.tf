27beaed5a3a480b012ceb34b204468f58573f48c
arkinfotech24 <161170306+arkinfotech24@users.noreply.github.com>
2025-05-01 22:56:51 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,15 @@ variable "vpc_id" {
   description = "The VPC ID where Account-SG exists"
   type        = string
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
