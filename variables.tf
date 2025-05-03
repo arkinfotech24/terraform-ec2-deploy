562b1497a654a70594a6c1c0b3f4344c9a559424
arkinfotech24 <161170306+arkinfotech24@users.noreply.github.com>
2025-05-03 02:38:55 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,42 @@
+# variables.tf
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
 variable "instance_type" {
-  default = "t2.micro"
+  description = "EC2 instance type"
+  default     = "t2.micro"
 }
 
-variable "instance_name" {
-  default = "Terraform-SSM-Instance"
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
 }
 
-variable "vpc_id" {
-  description = "The VPC ID where Account-SG exists"
-  type        = string
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  default     = "10.0.1.0/24"
 }
 
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
+# terraform.tfvars
+region              = "us-east-1"
+instance_type       = "t2.micro"
+vpc_cidr            = "10.0.0.0/16"
+public_subnet_cidr  = "10.0.1.0/24"
+
+# outputs.tf
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ec2.id
 }
 
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.ec2.public_ip
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
 }
