299e5fd71d35b84434716adf70768716baac8f90
arkinfotech24 <161170306+arkinfotech24@users.noreply.github.com>
2025-05-03 02:50:45 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,42 +1,12 @@
-# variables.tf
-variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
-}
-
 variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"
+  default = "t2.micro"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+variable "instance_name" {
+  default = "Terraform-Managed-Instance"
 }
 
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
-  default     = "10.0.1.0/24"
-}
-
-# terraform.tfvars
-region              = "us-east-1"
-instance_type       = "t2.micro"
-vpc_cidr            = "10.0.0.0/16"
-public_subnet_cidr  = "10.0.1.0/24"
-
-# outputs.tf
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.ec2.id
-}
-
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.ec2.public_ip
-}
-
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id
+variable "vpc_id" {
+  description = "The VPC ID where Account-SG exists"
+  type        = string
 }
