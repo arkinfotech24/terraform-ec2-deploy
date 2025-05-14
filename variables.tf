7cc9245e63b484dfdd02963c6a8d9689aa0168f6
arkinfotech24 <161170306+arkinfotech24@users.noreply.github.com>
2025-05-14 13:41:01 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,25 @@
 variable "instance_type" {
-  default = "t3.medium"
+  description = "EC2 instance type"
+  default     = "t3.micro"
 }
 
 variable "instance_name" {
-  default = "Terraform-Managed-Instance"
+  description = "Name tag for the EC2 instance"
+  default     = "web-ec2-ssm"
 }
 
-variable "vpc_id" {
-  description = "The VPC ID where Account-SG exists"
-  type        = string
-}
+
+#variable "instance_type" {
+#  default = "t3.medium"
+#}
+
+#variable "instance_name" {
+#  default = "Terraform-Managed-Instance"
+#}
+
+#variable "vpc_id" {
+#  description = "The VPC ID where Account-SG exists"
+#  type        = string
+#}
+
+
