b37ac6f6890d66512f6c3dbb19c72f1b3fc05304
arkinfotech24 <161170306+arkinfotech24@users.noreply.github.com>
2025-05-14 09:50:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,15 +45,15 @@ resource "aws_iam_instance_profile" "new_ssm_profile" {
 }
 
 # -----------------------------
-# Get Latest Amazon Linux 2023 AMI
+# Get Latest Ubuntu 22.04 AMI
 # -----------------------------
-data "aws_ami" "amazon_linux" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"]  # Canonical's AWS account ID
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*-x86_64"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
@@ -61,7 +61,7 @@ data "aws_ami" "amazon_linux" {
 # EC2 Instance with New IAM Role
 # -----------------------------
 resource "aws_instance" "web" {
-  ami                         = data.aws_ami.amazon_linux.id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = "alreliance-key"
   subnet_id                   = "subnet-9a5ab4d7"
@@ -72,7 +72,7 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
               #!/bin/bash
               cd /tmp
-              sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+              sudo apt update && sudo apt install -y amazon-ssm-agent
               sudo systemctl enable amazon-ssm-agent
               sudo systemctl start amazon-ssm-agent
             EOF
