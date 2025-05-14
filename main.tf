87840c464a0f4dc24070ba130a06999901cb8b28
arkinfotech24 <161170306+arkinfotech24@users.noreply.github.com>
2025-05-14 10:23:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,26 +47,13 @@ resource "aws_iam_instance_profile" "new_ssm_profile" {
 # -----------------------------
 # Get Latest Amazon Linux 2023 AMI
 # -----------------------------
-#data "aws_ami" "amazon_linux" {
-#  most_recent = true
-#  owners      = ["amazon"]
-
-#  filter {
-#    name   = "name"
-#    values = ["al2023-ami-*-x86_64"]
-#  }
-#}
-
-# -----------------------------
-# Get Latest Ubuntu 22.04 AMI
-# -----------------------------
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon_linux" {
   most_recent = true
-  owners      = ["099720109477"]  # Canonical's AWS account ID
+  owners      = ["amazon"]
 
   filter {
-    name   = "Ubuntu 22.04-ami-*-x86_64"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
   }
 }
 
