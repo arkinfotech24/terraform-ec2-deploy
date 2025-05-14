43d61ed29ad18e362bcc4d8c7349d4713caa95c8
arkinfotech24 <161170306+arkinfotech24@users.noreply.github.com>
2025-05-14 14:50:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,18 +21,19 @@ data "aws_security_group" "account_sg" {
 # -----------------------------
 resource "null_resource" "delete_existing_role" {
   provisioner "local-exec" {
-    command = <<EOT
-      set ROLE_NAME=SSM-EC2Role
-      aws iam get-role --role-name %ROLE_NAME% >nul 2>&1
-      if %ERRORLEVEL% EQU 0 (
-        echo ⚠️ Role %ROLE_NAME% exists. Deleting before Terraform Apply...
-        aws iam detach-role-policy --role-name %ROLE_NAME% --policy-arn arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore     
-        aws iam delete-role --role-name %ROLE_NAME%
-      ) else (
-        echo ✅ Role %ROLE_NAME% not found. Continuing...
-      )
-    EOT
+    command     = "${path.module}/scripts/delete-role.sh"
+    when        = "create"
+    interpreter = ["bash"]
+  }
+
+  provisioner "local-exec" {
+    command     = "${path.module}/scripts/delete-role.cmd"
+    when        = "create"
     interpreter = ["cmd.exe", "/C"]
+  }
+
+  triggers = {
+    always_run = timestamp()
   }
 }
 
