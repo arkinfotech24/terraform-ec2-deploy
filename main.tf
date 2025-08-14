64614bb9fb268d86aa3e81d07978f2fe5cbd15a6
arkinfotech24 <161170306+arkinfotech24@users.noreply.github.com>
2025-08-14 14:26:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
+#provider "aws" {
+#  region = "us-east-1"
+#}
+
 provider "aws" {
-  region = "us-east-1"
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 # -----------------------------
